d10f055ed40e35947617f42f570e8ed3a3b966b0
abayomi igwubor <igwubor@outlook.com>
2020-01-06 00:08:32 +0100

    first commit

---
+++ src/Components/General.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class General extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default General;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Test() {
+	return (
+		<div className="container">
+			<div className="jumbotron bg-success">
+				<center>
+					<h1 className="text-white">NairaLand Forum</h1>
+				</center>
+
+				<Link className="text-white" to="/">
+					{' '}
+					Lastest News
+				</Link>
+                <ul>
+                <Link to=''>
+                <li className="text-white">General</li>
+                </Link>
+                    <Link to='/sports'>
+                    <li className="text-white">Sports</li>
+                    </Link>
+                    <Link to='/science'>
+                    <li className="text-white">Science</li>
+                    </Link>
+                    <Link to='/technology'>
+                    <li className="text-white">Technology</li>
+                    </Link>
+                   
+                    <Link to='/business'>
+                    <li className="text-white">Business</li>
+                    </Link>
+                    <Link to='entertainment'>
+                    <li className="text-white">Entertainment</li>
+                    </Link>
+					
+                    <Link to="/health">
+                    <li className="text-white">Health</li>
+                    </Link>
+					
+				</ul>
+			</div>
+		</div>
+	);
+}
+export default Test;
